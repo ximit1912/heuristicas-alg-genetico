ead3fd0b1c6d36a7c0c03775a3a1fb8d2b523528
Vinicius Schmitz <viniciusschmitzwork@gmail.com>
2025-01-26 12:55:28 -0300

    Avaliacao e selecao

--- main.c
+++ main.c
@@ -5,13 +5,11 @@
 
 #define TAM_POPULACAO 5 /* CORRESPONDE A QNT DE SOLUCOES DE UMA POPULACAO */
 #define TAM_SOLUCAO 10 /* TAMANHO DA INSTANCIA, CORRESPONDE A UM CROMOSSOMO/INDIVIDUO/SOLUCAO INICIAL TRAÇANDO PARALELO COM ALGORITMOS GENETICOS */
-// #define SOLUCAO_IDEAL 36905 /* MELHOR CUSTO CONHECIDO PARA A INSTANCIA, CORRESPONDE AO INDIVIDUO MODELO IDEAL */
+#define SOLUCAO_IDEAL 1000 /* MELHOR CUSTO CONHECIDO PARA A INSTANCIA, CORRESPONDE AO INDIVIDUO MODELO IDEAL */
 
-int **populacaoAtual, **proxPopulacao; /* População atual, Temporariamente a proxima populacao */
-int *fitness; /*  */
-int solucaoIdeal; /* Melhor custo conhecido da literatura para a instancia  */
+int **populacaoAtual, **proxPopulacao, classificacao[TAM_POPULACAO]; /* População atual, Temporariamente a proxima populacao */
 
-unsigned long long int distTotal[TAM_POPULACAO], distAux;
+unsigned long long int distTotal[TAM_POPULACAO], distAux, *fitness;
 
 // Estrutura para guardar as cidades do arquivo
 typedef struct{
@@ -27,30 +25,24 @@ Cidades cidades[TAM_SOLUCAO]; /* vetor contendo as cidades */
 void limparVisitados();
 void mostrarSolucao(int *);
 int calculaDistancia(Cidades , Cidades );
-void lerArquivo(char *); /* Funções código anterior */
+void lerArquivo(char *); 
 void buscaLocalVizinhoMaisProximo(int *, int);
+// Funções novas secundárias
+void mostrarPopulacao(void);
 
 
-
-// Novas funções e hierarquia entre elas
+//Funções novas  e hierarquia entre elas
 void gerarNovaSolucao(int *, int*, int);
-void alocarMemoria(void);
-void inicializarPopulacao(void);
-    void buscaLocalVizinhoMaisProximo(int*, int); /* Gerar 1° solucao */
-    void geraNovaSolucao();
-        void embaralhaSolucao(int *solucao);
+void alocarMemoria(void);            /* OK */
+void inicializarPopulacao(void);     /* OK */
+    void buscaLocalVizinhoMaisProximo(int*, int); /* Gerar 1° solucao variando alatoriamente o nó inicial*/
 int rodarAlgoritmoGenetico(void);
-    int iniciarAvaliacao(void);
+    void iniciarAvaliacao(void);         /* OK - Avalia as distancias totais da populacao para calcular fitness, retorna o melhor */
     void iniciarSelecao(void);
     void iniciarCruzamento(void);
     void iniciarMutacao(void);
 
-
-
-
-
-
-
+/*=============================================================================================*/
 
 
 
@@ -61,77 +53,97 @@ void main(int argc, char *argv[]){
     lerArquivo(argv[1]);
     alocarMemoria();
     
-    printf("\n->Inicializando populacao ...");
+    printf("\n-> Inicializando populacao ...");
     inicializarPopulacao();
-    printf("\n<-Populacao inicializada!!");
-
-    for (int i = 0; i < TAM_POPULACAO; i++)
-    {   
-        printf("\nDistTotal: %lld | Sol[%d]: ", distTotal[i], i);
-        mostrarSolucao(populacaoAtual[i]);
-    }
+    printf("<- Populacao inicializada!!\n\n");
     
 
-    /*
-        int main(){
-            int finalGeneration;
-            AllocateMemory();
-            finalGeneration = DoOneRun();
-            printf("The final generation was: %d\n", finalGeneration);
+    iniciarAvaliacao(); /* Avalia o fitness */
+    iniciarSelecao();   /* Ordena os melhores com base no fitness */
+    mostrarPopulacao(); /* Imprimir resultados */
+
+
+
+// MODELO BASE: aqui sao geradas novas populacoes e é feito a avaliacao, selecao, cruzamento e mutacao ATÉ encontrar um fitness 0, ou seja, individuo perfeito
+    // O QUE ALTERAR: definir 1 ou + criterios de parada mais viaveis para o tsp    
+    /* 
+        int DoOneRun(void){
+            int generations = 1;
+            int perfectGeneration = 0;
+
+            InitializeOrganisms();
+
+            while(1){
+                perfectGeneration = EvaluateOrganisms();
+                if( perfectGeneration==TRUE ) return generations;
+                ProduceNextGeneration();
+                ++generations;
+            }
         }
     */
 }
 
 
 
+
+void iniciarSelecao()
+{
+    int temp;
+    int usado[TAM_POPULACAO] = {0}; // Array para marcar índices já usados
+
+    for (int i = 0; i < TAM_POPULACAO; i++)
+    {   
+        temp = -1;
+
+        // Encontra o índice do menor fitness que ainda não foi usado
+        for(int j = 0; j < TAM_POPULACAO; j++){
+            if (!usado[j] && (temp == -1 || fitness[j] < fitness[temp])){
+                temp = j;
+            }
+        }
+
+        // Marca o índice como usado e o adiciona à classificação
+        usado[temp] = 1;
+        classificacao[i] = temp;
+    }
+
+    printf("-> Mostrando classificacao\n\t");
+    for (int i = 0; i < TAM_POPULACAO; i++)
+    {
+        printf("%d, ", classificacao[i]);
+    }
+    printf("\n<- \n\n");
+}
+
+void iniciarAvaliacao()
+{
+    for (int i = 0; i < TAM_POPULACAO; i++)
+    {
+        fitness[i] = distTotal[i] - SOLUCAO_IDEAL;
+    }
+
+}   
+
 // INICIALIZA A POPULACAO
-void inicializarPopulacao(){
+void inicializarPopulacao()
+{
     srand(1); // seed a mesma para testes
 
-    for(int i = 0; i < TAM_POPULACAO; i++){   // Inicializa os outros individuos da populacao a partir do 1° individuo
+    printf("\n\t-> Iniciando vizinho mais proximo para cada individuo ...");
+    for(int i = 0; i < TAM_POPULACAO; i++){   // Inicializa a população com construtiva vizinho mais proximo, alterando o nó inicial para gerar variação
         buscaLocalVizinhoMaisProximo(populacaoAtual[i], i);
     }
+    printf("\n\t<- Vizinho mais proximo concluido!!\n");
 }
-
-/*
-// GERA NOVA SOLUCAO A PARTIR DE OUTRA
-void gerarNovaSolucao(int *solucaoOrigem, int *solucaoDestino, int id){
-    int i = 0;
-
-    for (i = 0; i < TAM_SOLUCAO; i++){
-        solucaoDestino[i] = solucaoOrigem[i];
-    }
-
-    embaralhaSolucao(solucaoDestino);
-
-    distTotal[id] = 0;
-    for(i = 1; i < TAM_SOLUCAO; i++){
-        distTotal[id] += calculaDistancia(cidades[solucaoDestino[i-1] - 1], cidades[solucaoDestino[i] - 1]);
-    }
-    distTotal[id] += calculaDistancia(cidades[solucaoDestino[i-1] - 1], cidades[solucaoDestino[0] - 1]);
-}
-
-void embaralhaSolucao(int *solucao){
-    int j, temp;
-
-    for (int i = TAM_SOLUCAO-1; i > 0; i--) /* Algoritmo de Fisher-Yates para embaralhar a solucao e a diferenciar da original 
-    {
-        j = rand() % (i+1); /* gera um indice aleatorio entre 0 e i para ser trocado a partir do ultimo indice 
-        temp = solucao[i];
-        solucao[i] = solucao[j];
-        solucao[j] = temp;
-    }
-}
-*/
-
 
 // aloca memoria para as populacoes de solucoes 
-void alocarMemoria(void){
+void alocarMemoria(void)
+{
     int solucao;
 
     populacaoAtual = (int**) malloc(sizeof(int*) * TAM_POPULACAO);
     proxPopulacao =  (int**) malloc(sizeof(int*) * TAM_POPULACAO);
-    fitness = (int*) malloc(sizeof(int) * TAM_POPULACAO);
+    fitness = (unsigned long long int*) malloc(sizeof(unsigned long long int) * TAM_POPULACAO);
     
     for (solucao = 0; solucao < TAM_POPULACAO; solucao++)
     {
@@ -140,6 +152,18 @@ void alocarMemoria(void){
     }
 }
 
+// Impressão de resultados
+void mostrarPopulacao()
+{
+    printf("-> Mostrando populacao\n");
+
+    for (int i = 0; i < TAM_POPULACAO; i++)
+    {   
+        printf("\tDistTotal: %lli | Fitness: %lli | Sol[%d]: ", distTotal[i], fitness[i], i);
+        mostrarSolucao(populacaoAtual[i]);
+    }
+    printf("<- \n\n");
+}
 
 /*=============================================================================================*/ 
 // FUNCOES DO CODIGO ANTERIOR 
@@ -155,7 +179,6 @@ void buscaLocalVizinhoMaisProximo(int *solucao, int idSolucao)
     cidades[noAtual].visitado = 1;
     solucao[0] = noAtual+1; /* OBS: cidades vão de 1 até TAMSOLUCAO */
 
-    printf("\n->Iniciando vizinho mais proximo ...");
     while (i < TAM_SOLUCAO - 1)
     {
         distAux = __INT_MAX__;
@@ -180,6 +203,7 @@ void buscaLocalVizinhoMaisProximo(int *solucao, int idSolucao)
         }
         cidades[vizMaisProx].visitado = 1;
         distTotal[idSolucao] += distAux;
+                
         // printf("##viz escolhido: %d & distancia total: %d \n\n", vizMaisProx+1, distTotal);
 
         solucao[++i] = vizMaisProx+1;
@@ -189,7 +213,7 @@ void buscaLocalVizinhoMaisProximo(int *solucao, int idSolucao)
     // solucao[++i] = noInicial+1; /* REMOVIDO */ 
     
     // printf("\nDistancia total para individuo/solucao %d: %lld\n\n", idSolucao, distTotal[idSolucao]);
-    printf("\n<-Vizinho mais proximo concluido!!\n\n");
+
 
     limparVisitados();
 }
@@ -232,7 +256,7 @@ void lerArquivo(char *nome)
     }
     else
     {
-        printf("\n->Arquivo aberto com sucesso!\n ");
+        printf("\n<- Arquivo aberto com sucesso!\n ");
 
         int i = 0, idAux;
         float x,y;
@@ -259,3 +283,39 @@ void lerArquivo(char *nome)
 
     fclose(arq);
 }
+
+
+
+
+/*
+// GERA NOVA SOLUCAO A PARTIR DE OUTRA
+void gerarNovaSolucao(int *solucaoOrigem, int *solucaoDestino, int id){
+    int i = 0;
+
+    for (i = 0; i < TAM_SOLUCAO; i++){
+        solucaoDestino[i] = solucaoOrigem[i];
+    }
+
+    embaralhaSolucao(solucaoDestino);
+
+    distTotal[id] = 0;
+    for(i = 1; i < TAM_SOLUCAO; i++){
+        distTotal[id] += calculaDistancia(cidades[solucaoDestino[i-1] - 1], cidades[solucaoDestino[i] - 1]);
+    }
+    distTotal[id] += calculaDistancia(cidades[solucaoDestino[i-1] - 1], cidades[solucaoDestino[0] - 1]);
+}
+*/
+
+/*
+void embaralhaSolucao(int *solucao){
+    int j, temp;
+
+    for (int i = TAM_SOLUCAO-1; i > 0; i--) /* Algoritmo de Fisher-Yates para embaralhar a solucao e a diferenciar da original 
+    {
+        j = rand() % (i+1); /* gera um indice aleatorio entre 0 e i para ser trocado a partir do ultimo indice 
+        temp = solucao[i];
+        solucao[i] = solucao[j];
+        solucao[j] = temp;
+    }
+}
+*/
