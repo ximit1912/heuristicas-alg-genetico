374f91e38c3d8d63166ae6dc81ef8cf50ffb569e
Vinicius Schmitz <viniciusschmitzwork@gmail.com>
2025-01-26 17:45:25 -0300

    Crossover e mutacao

--- main.c
+++ main.c
@@ -22,14 +22,17 @@ Cidades cidades[TAM_SOLUCAO]; /* vetor contendo as cidades */
 /*=============================================================================================*/
 
 // Funções do código anterior
-void limparVisitados();
+void limparVisitados(void);
 void mostrarSolucao(int *);
 int calculaDistancia(Cidades , Cidades );
 void lerArquivo(char *); 
 void buscaLocalVizinhoMaisProximo(int *, int);
 // Funções novas secundárias
 void mostrarPopulacao(void);
-
+void cortarSegmento(int*, int*);
+int verificaInseridoFilho(int*, int, int);
+unsigned long long int calculaDistTotal(int*);
+long int iniciarPairswap(int*);
 
 //Funções novas  e hierarquia entre elas
 void gerarNovaSolucao(int *, int*, int);
@@ -38,9 +41,9 @@ void inicializarPopulacao(void);     /* OK */
     void buscaLocalVizinhoMaisProximo(int*, int); /* Gerar 1° solucao variando alatoriamente o nó inicial*/
 int rodarAlgoritmoGenetico(void);
     void iniciarAvaliacao(void);         /* OK - Avalia as distancias totais da populacao para calcular fitness, retorna o melhor */
-    void iniciarSelecao(void);
-    void iniciarCruzamento(void);
-    void iniciarMutacao(void);
+    void iniciarSelecao(void);           /* OK - Classifica para Seleção em ordem da melhor para a pior solução*/
+    unsigned long long int* iniciarCruzamentoOX1(int*, int*);    /* OK - Faz cruzamento entre os 2 melhores com método OX1*/
+    void iniciarMutacao(int *, int*, unsigned long long int*);               /*  */
 
 /*=============================================================================================*/
 
@@ -48,7 +51,7 @@ int rodarAlgoritmoGenetico(void);
 
 void main(int argc, char *argv[]){
     clock_t inicio, fim;
-    
+    int iteracoes = 1; /* Criterio de parada */
 
     lerArquivo(argv[1]);
     alocarMemoria();
@@ -57,35 +60,221 @@ void main(int argc, char *argv[]){
     inicializarPopulacao();
     printf("<- Populacao inicializada!!\n\n");
     
+    do {
+        iniciarAvaliacao(); /* Avaliação: avalia o fitness */
+        iniciarSelecao();   /* Seleção: ordena os melhores com base no fitness */
+        mostrarPopulacao(); /* Imprimir resultados */
+        
+        int filho1[TAM_SOLUCAO] = {0},
+            filho2[TAM_SOLUCAO] = {0};
+        unsigned long long int *novasDists =  
+        iniciarCruzamentoOX1(filho1, filho2); /* Reprodução: cross-over OX1*/
 
-    iniciarAvaliacao(); /* Avalia o fitness */
-    iniciarSelecao();   /* Ordena os melhores com base no fitness */
-    mostrarPopulacao(); /* Imprimir resultados */
+        iniciarMutacao(filho1, filho2, novasDists);
+
+        iteracoes--;
+    }while (iteracoes > 0);
+
+}
+
+void iniciarMutacao(int *filho1, int *filho2, unsigned long long int *novasDists)
+{
+        long int diffMutacao1, diffMutacao2;
+        printf("\n-> Iniciando Mutacao para filhos ...");
+        diffMutacao1 = iniciarPairswap(filho1);
+        diffMutacao2 = iniciarPairswap(filho2);
 
 
-
-// MODELO BASE: aqui sao geradas novas populacoes e é feito a avaliacao, selecao, cruzamento e mutacao ATÉ encontrar um fitness 0, ou seja, individuo perfeito
-    // O QUE ALTERAR: definir 1 ou + criterios de parada mais viaveis para o tsp    
-    /* 
-        int DoOneRun(void){
-            int generations = 1;
-            int perfectGeneration = 0;
-
-            InitializeOrganisms();
-
-            while(1){
-                perfectGeneration = EvaluateOrganisms();
-                if( perfectGeneration==TRUE ) return generations;
-                ProduceNextGeneration();
-                ++generations;
-            }
+        if (!diffMutacao1){
+            printf("\n\t Nao melhorou: ---------- | Filho 1: ");
         }
-    */
+        else
+            printf("\n\t NovaDistTotal: %lli (-%li) | Filho 1: ",  novasDists[0] - diffMutacao1, diffMutacao1); mostrarSolucao(filho1); 
+        
+        
+        if (!diffMutacao2){
+            printf("\t Nao melhorou: ---------- | Filho 2: ");
+        }
+        else
+            printf("\t NovaDistTotal: %lli (-%li) | Filho 2: ", novasDists[1] - diffMutacao2, diffMutacao2); mostrarSolucao(filho2); 
+
+        printf("<- Mutacao finalizada !\n");   
+}
+
+// Faz Mutação com pair-swap, retorna a diferença do custo total
+long int iniciarPairswap(int *solucao)
+{
+    int noFixo = (rand() % (TAM_SOLUCAO-1)) + 1, /* produz valores aleatorios de 1 a (TAMSOL-1), para nao ser a origem */
+        melhorou = 0,  
+        i = TAM_SOLUCAO-1, 
+        temp;
+    unsigned long long int distNova, distAntiga, distVizinhosNoFixo;
+    long int distDiferenca = 0;
+    
+    
+    // printf("\n\nFirst-Improvement iniciada: pair-swap de %d\n", noFixo);
+
+    // Calcula as distancias para os vizinhos do noFixo (1° elemento do par)
+    distVizinhosNoFixo = calculaDistancia(cidades[solucao[noFixo - 1]-1], cidades[solucao[noFixo]-1]);
+    distVizinhosNoFixo += calculaDistancia(cidades[solucao[noFixo]-1], cidades[solucao[noFixo + 1]-1]);
+    
+    while(!melhorou && i > 0)
+    {
+        
+        if(i != noFixo && i != noFixo+1 && i != noFixo-1)
+        {   
+            // Recalcula as distancias para os vizinhos do noFixo (1° elemento do par)
+            distAntiga = distVizinhosNoFixo;
+            // Calcula as distancias para os vizinhos do i (2° elemento do par)
+            distAntiga += calculaDistancia(cidades[solucao[i - 1]-1], cidades[solucao[i]-1]);
+            distAntiga += calculaDistancia(cidades[solucao[i]-1], cidades[solucao[i + 1]-1]);
+
+            // Calcula as novas distancias para os novos vizinhos de i "swappado" (antes vizinhos de noFixo)
+            distNova = calculaDistancia(cidades[solucao[noFixo - 1]-1], cidades[solucao[i]-1]);
+            distNova += calculaDistancia(cidades[solucao[i]-1], cidades[solucao[noFixo + 1]-1]);
+            // Calcula as novas distancias para os novos vizinhos de noFixo "swappado" (antes vizinhos de i)
+            distNova += calculaDistancia(cidades[solucao[i - 1]-1], cidades[solucao[noFixo]-1]);
+            distNova += calculaDistancia(cidades[solucao[noFixo]-1], cidades[solucao[i + 1]-1]);
+
+            // printf("Trocando %d e %d, distAntiga: %lld, distNova: %lld\n", solucao[noFixo], solucao[i], distAntiga, distNova);
+            if(distNova < distAntiga)
+            {   
+                // Atualiza a distancia/custo total 
+                distDiferenca -= distAntiga;
+                distDiferenca += distNova;
+                melhorou = 1;
+                
+                // Faz a troca do par no conjunto solucao 
+                temp = solucao[noFixo];
+                solucao[noFixo] = solucao[i];
+                solucao[i] = temp;
+            }
+        }    
+
+        i--;
+    }
+
+    if(!melhorou)
+        return 0;
+    else
+    {
+        // printf("#Encontrado!\n\n#Pair-swap de conjSol[%d]=%d e conjSol[%d]=%d encontrou uma solucao melhor!", noFixo, solucao[i], i, solucao[noFixo]);
+        // printf("\n#Diferenca: %ld\n", distDiferenca);
+        return -distDiferenca;
+    }
+}
+
+// Faz Cross-over OX1
+unsigned long long int* iniciarCruzamentoOX1(int *filho1, int *filho2)
+{
+    printf("-> Inicializando Crossover OX1 ...\n");
+    srand(time(NULL));               /* Seed fixa para testes */
+        
+    int pai1 = classificacao[0], 
+        pai2 = classificacao[1],   /* Escolhe as 2 melhores solucoes como pais para o Cross-over */
+        pontoCorte1, pontoCorte2, tamSegmento;
+
+    cortarSegmento(&pontoCorte1, &pontoCorte2); /* Corta 2 pontos para escolher 1 segmento */
+    tamSegmento = pontoCorte2 - pontoCorte1 + 1;
+    printf("\tPontos de corte: %d - %d (%dun)", pontoCorte1, pontoCorte2, tamSegmento);
+
+    int inseridosFilho1[tamSegmento],
+        inseridosFilho2[tamSegmento],
+        j = 0;
+    for(int i = pontoCorte1; i <= pontoCorte2; i++){    /* Copia os segmentos dos pais para os filhos */
+        filho1[i] = populacaoAtual[pai1][i],
+        filho2[i] = populacaoAtual[pai2][i];
+        
+        inseridosFilho1[j] = filho1[i],
+        inseridosFilho2[j++] = filho2[i];
+    }
+
+    j = pontoCorte2 + 1;
+    // Preenche os vértices do filho 1 copiando os vértices do pai 2 que ainda nao foram inseridos do segmento, em ordem
+    for(int i = pontoCorte2 + 1; i != pontoCorte1;){
+        if(i == TAM_SOLUCAO)
+            i = 0;
+        if(j == TAM_SOLUCAO)
+            j = 0;
+
+        if(!verificaInseridoFilho(inseridosFilho1, populacaoAtual[pai2][j], tamSegmento)){
+            filho1[i] = populacaoAtual[pai2][j];
+            i++;
+        }
+        j++;
+    }
+
+    j = pontoCorte2 + 1;
+    // Preenche os vértices do filho 2 copiando os vértices do pai 1 que ainda nao foram inseridos do segmento, em ordem
+    for(int i = pontoCorte2 + 1; i != pontoCorte1;){
+        if(i == TAM_SOLUCAO)
+            i = 0;
+        if(j == TAM_SOLUCAO)
+            j = 0;
+
+        if(!verificaInseridoFilho(inseridosFilho2, populacaoAtual[pai1][j], tamSegmento)){
+            filho2[i] = populacaoAtual[pai1][j];
+            i++;
+        }
+        j++;
+    }
+
+    unsigned long long int* novasDists = (unsigned long long int*)malloc(2 * sizeof(unsigned long long int));
+    novasDists[0] = calculaDistTotal(filho1);
+    novasDists[1] = calculaDistTotal(filho2);
+    printf("\n\tDistTotal: %lli | ", novasDists[0]); printf("Filho 1: "); mostrarSolucao(filho1); 
+    printf("\tDistTotal: %lli | ", novasDists[1]); printf("Filho 2: "); mostrarSolucao(filho2);
+
+    printf("<- Crossover-OX1 finalizado !\n");
+
+    return novasDists;
+}
+
+// Retorna custo de uma solucao
+unsigned long long int calculaDistTotal(int *solucao)
+{
+    distAux = 0;
+    int i;
+    for (i = 1; i < TAM_SOLUCAO; i++)
+    {   
+        distAux += calculaDistancia(cidades[solucao[i-1] - 1], cidades[solucao[i] - 1]);
+    }
+    
+    return distAux += calculaDistancia(cidades[solucao[i-1] - 1], cidades[solucao[0] - 1]);
+}
+
+// Verifica se um no já foi inserido no filho
+int verificaInseridoFilho(int *inseridosFilho, int no, int tamSegmento)
+{
+    for(int i = 0; i < tamSegmento; i++){
+        if (inseridosFilho[i] == no)
+            return 1;
+    }
+
+    return 0;
+}
+
+// Define 2 pontos de corte para um segmento
+void cortarSegmento(int *p1, int *p2)
+{
+    int pontoCorte1 = (rand() % (TAM_SOLUCAO-1) + 1) , /* Define os pontos de corte, se forem iguais, calcula de novo o 2°*/
+        pontoCorte2 = rand() % TAM_SOLUCAO;
+    while (pontoCorte1 == pontoCorte2)
+        pontoCorte2 = rand() % TAM_SOLUCAO;
+    
+    if (pontoCorte1 > pontoCorte2)
+    {
+        int temp = pontoCorte2;
+        pontoCorte2 = pontoCorte1;
+        pontoCorte1 = temp;   
+    }
+
+    *p1 = pontoCorte1,
+    *p2 = pontoCorte2;
 }
 
 
-
-
+// Faz Seleção
 void iniciarSelecao()
 {
     int temp;
@@ -168,7 +357,12 @@ void mostrarPopulacao()
 /*=============================================================================================*/ 
 // FUNCOES DO CODIGO ANTERIOR 
 
-// CONSTRUTIVA VIZINHO MAIS PROXIMO
+
+
+
+
+
+// BUSCA LOCAL VIZINHO MAIS PROXIMO
 void buscaLocalVizinhoMaisProximo(int *solucao, int idSolucao)
 {
     distTotal[idSolucao] = 0, distAux = 0;
